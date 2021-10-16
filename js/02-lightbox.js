fea8378ab8a0d41c73f8f535d951206b4dcf7fc3
Anna Samsonenko <foster.anna93@gmail.com>
2021-10-17 00:20:01 +0300

    add solved task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryCardsMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const isGalleryCardEl = event.target.classList.contains("gallery__image");
+  if (!isGalleryCardEl) {
+    return;
+  }
+
+  openModal();
+}
+
+const settingsForModal = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+function openModal() {
+  const modal = new SimpleLightbox(".gallery__item", settingsForModal);
+  modal.on("closed.simplelightbox", () => {
+    modal.refresh();
+  });
+}
